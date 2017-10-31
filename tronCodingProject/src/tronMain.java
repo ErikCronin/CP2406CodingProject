61ac6ecc47abe88c2f05b524ca7b10b0f690ced2
ErikCronin <strandeddata@gmail.com>
2017-10-31 17:36:44 +1000

    changed user input to come in through a java window rather than terminal

--- tronCodingProject/src/tronMain.java
+++ tronCodingProject/src/tronMain.java
@@ -1,90 +1,80 @@
-import java.util.Scanner;
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class tronMain {
     public static void main(String[] args)
     {
         /* Base code for the game will go here, things such as: */
-
         /* Booting the game, calling to other functions and GUI */
-
         /* Will receive each player name as they are input from player() function */
-        lightCycle();
-        boolean playerBoolean = false;
+        String lcColour = lightCycle();
         String player = createPlayer();
-        System.out.println("Hello " + player + "!");
-
-        /* Sends winning player's name and jet wall length to leaderboard */
+        /* Sends winning player's name and jet wall length to leader board */
 
         /* Creates grid based on user input */
         /* Grid width creation */
         String gridWidth = "width";
         int createGridX = createGrid(gridWidth);
-
         /* Grid height creation */
         String gridHeight = "height";
         int createGridY = createGrid(gridHeight);
 
-        System.out.println("Your grid size is: " + createGridX + " by " + createGridY);
+        /* Startup Menu completion */
+        JOptionPane.showMessageDialog(null, "Hello " + player + "!\nYou will have a(n) " + lcColour + " light cycle.\nYour grid size is: " + createGridX + " by " + createGridY, "Game Setup", JOptionPane.PLAIN_MESSAGE);
     }
 
-    public static int createGrid(String widthOrHeight)
+    private static int createGrid(String widthOrHeight)
     {
         int createGrid;
-        System.out.println("Please input your grid " + widthOrHeight + " (Between 500 and 10,000)");
-        Scanner gridInput = new Scanner(System.in);
-        createGrid = gridInput.nextInt();
+        String gridInput = JOptionPane.showInputDialog("Enter your " + widthOrHeight + ". (Between 500 and 10,000)");
+        createGrid = Integer.parseInt(gridInput);
 
         /* Grid error check */
         if(createGrid < 500 || createGrid > 10000)
         {
             while(createGrid < 500)
             {
-                System.out.println("Sorry, that was too low. Please reinput " + widthOrHeight + " (Between 500 and 10,000)");
-                gridInput = new Scanner(System.in);
-                createGrid = gridInput.nextInt();
+                gridInput = JOptionPane.showInputDialog("Sorry that was too low. Please reinput " + widthOrHeight + ". (Between 500 and 10,000)");
+                createGrid = Integer.parseInt(gridInput);
             }
 
             while(createGrid > 10000)
             {
-                System.out.println("Sorry, that was too high. Please reinput " + widthOrHeight + " (Between 500 and 10,000)");
-                gridInput = new Scanner(System.in);
-                createGrid = gridInput.nextInt();
+                gridInput = JOptionPane.showInputDialog("Sorry that was too high. Please reinput " + widthOrHeight + ". (Between 500 and 10,000)");
+                createGrid = Integer.parseInt(gridInput);
             }
         }
         else
         {
-            System.out.println("Thank you!");
         }
 
         return createGrid;
     }
 
-    public static String createPlayer()
+    private static String createPlayer()
     {
         /* Ask for player name. */
-        System.out.println("Please input you're name: ");
-        Scanner nameInput = new Scanner(System.in);
-        return nameInput.nextLine();
+        String nameInput = JOptionPane.showInputDialog("Please enter your name: ");
+        return nameInput;
 
         /* If first player input, how many players? (must be at least 2) */
     }
 
 
-    public static void leaderboard(String player, int jetLength)
+    /* private static void leaderBoard(String player, int jetLength)
     {
-        /* receives winning players name and jet wall length, and puts it on a leaderboard. */
-        /* Will store winning player's name and jet wall length from current and previous games */
+        receives winning players name and jet wall length, and puts it on a leader board.
+        Will store winning player's name and jet wall length from current and previous games
     }
+    */
 
-    public static void lightCycle()
+    private static String lightCycle()
     {
         /* Must always be facing inwards away from grid */
         /* Given random colour from a selection of 9 (red, green, blue, black, white, orange, pink, yellow, purple) */
         Random dice = new Random();
         int number;
         number = dice.nextInt(8);
-        System.out.println(number + " ");
         String colour = "";
         if(number == 0)
         {
@@ -131,8 +121,8 @@ public class tronMain {
             colour = "purple";
         }
 
-        System.out.println("Your light cycle colour is: " + colour);
+        return colour;
         /* cannot stop once cycle has started, can speed up and slow down */
-        /* Lightcycle and jet trail are the same colour */
+        /* Light cycle and jet trail are the same colour */
     }
 }
